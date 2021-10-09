b9331b247c415dd10d126ed279bf99e9ee89166f
the-electro-punk <bwr2555@hotmail.com>
2021-10-08 19:04:38 -0500

    changed to writefile

--- assets/RMgen.js
+++ assets/RMgen.js
@@ -1,6 +1,6 @@
 var bio = require('bio')
 
-bio.appendFile('name', 'age', 'height', function (err) {
+bio.writeFile('name', 'age', 'height', function (err) {
     if (err) throw err;
     console.log('saved')
 })
