47809df0ed3527ad92231e8e2a58ea6196155388
the-electro-punk <bwr2555@hotmail.com>
2021-10-17 21:31:17 -0500

    finalized code

--- assets/index-updated.js
+++ assets/index-updated.js
@@ -1,52 +1,120 @@
-// TODO: Include packages needed for this application
+// allows me to use the file system
+const fs = require('fs')
+// allows me to use inquirer to ask questions
 const inquirer = require('inquirer');
 
-inquirer.prompt ([
+// inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer));
+
+
+// this asks the questions from the command line to gather data
+inquirer.prompt (
+    // this is supposed to cause the questions to loop and add more if needed
+    // type: 'loop',
+    // name: 'items',
+    // message: 'add something else?',
+    // questions: 
+    [
+
+    // it appears prompt only allows 3 questions
     {
     type: 'input', 
     name:"input_type", 
     message:'what is filename.md?',
-    default:'filename.md',
+    validate: (answer) => {
+        if(answer === '') {
+            return 'please enter a valid name'
+        }
+        return true
+        }
     },
     {
-    type:'list',
-    name:'list_question',
-    message: 'what programing language do you like?',
-    choices:['Javascript','C++','Java','python'],
-    default: 'Javascript'
+    type:'input',
+    name:'check_question',
+    message: 'what will the header be?',
+    validate: (answer) => {
+        if(answer === '') {
+            return 'please enter a valid name'
+        }
+        return true
+        }
     },
     {
-    type:'checkbox',
-    name:'checkbox_question',
-    message: 'how many programing languages do you speak?',
-    choices:['Javascript','C++','Java','python'],
-    default: 'Javascript'
+    type:'input',
+    name:'content_question',
+    message: 'what will the ReadMe file contain?',
+    validate: (answer) => {
+        if(answer === '') {
+            return 'please enter a valid name'
+        }
+        return true
+        }
     },
+    // {
+    //     type:'confirm',
+    //     name: 'askAgain',
+    //     message: 'is there more to add?',
+    //     default: false
+    // },
 ]).then(answers => {
+    const fileName = answers.input_type;
+    const table = answers.check_question;
+    const content = answers.content_question;
+    const fullstring = '# '+ table + '\n' + content
+
+    // this adds the content to a new file
     console.log(answers)
+    fs.appendFile(fileName, fullstring, function (err) {
+        if (err) throw err;
+        console.log('saved')
+    })
 })
 
-// TODO: Create an array of questions for user input
 
-// const questions = [{type: 'input', id:'main', message:'what is filename.md?'},{type: 'input', id:'content', message:'what will be the content?'},{type: 'input', id:'tasks', message:'what are the tasks'}];
-// const answers = {}
+// inquirer.prompt ([
+//     // it appears prompt only allows 3 questions
+//     {
+//     type: 'list', 
+//     name:"repeat_type", 
+//     message:'is there more content?',
+//     choices:['yes','no'],
+//     },
+//     {
+//     type:'input',
+//     name:'check_question',
+//     message: 'what will the table of contents include?',
+//     validate: (answer) => {
+//         if(answer === '') {
+//             return 'please enter a valid name'
+//         }
+//         return true
+//         }
+//     },
+//     {
+//     type:'input',
+//     name:'content_question',
+//     message: 'what will the ReadMe file contain?',
+//     validate: (answer) => {
+//         if(answer === '') {
+//             return 'please enter a valid name'
+//         }
+//         return true
+//         }
+//     },
+// ]).then(answers => {
+//     const repeat = answers.repeat_type;
+//     const table = answers.check_question;
+//     const content = answers.content_question;
+//     const fullstring = '# '+ table + '\n\n' + content
 
-// TODO: Create a function to write README file
+//     // console.log('file name is '+ fileName)
+//     // console.log('known programs include '+ content)
+//     console.log(answers)
 
-// function writeToFile(fileName, content, tasks) {
-//     fileName = questions[0]
-//     content = questions[1]
-//     tasks = questions[2]
-//     console.log('fileName is ' + fileName)
-// }
+//     fs.writeFile(fileName, fullstring, function (err) {
+//         if (err) throw err;
+//         console.log('saved')
+//     })
+// })
+// add more prompts???
 
-// TODO: Create a function to initialize app
-function init() {
-
-}
-
-// Function call to initialize app
-init();
-
-
-// https://www.youtube.com/watch?v=1AxFrY2oSiw&ab_channel=Markodex
+// C:\Users\Ben\Desktop\_Coding BootCamp\_weeks lessons\week 9\node-assignment\Readme-Generator\assets\inquirer-practice.js
